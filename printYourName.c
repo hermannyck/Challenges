9194d69af956da4faa6318715195df78cd66a8da
hermannyck <119290220+hermannyck@users.noreply.github.com>
2022-12-13 10:05:22 +0300

    Update printYourName.c

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,18 @@
 // your name surname and age from user
+
+#include <stdio.h>
+
+int main() {
+  int age;
+  char name[10], surname[10];
+   printf("Enter your name \n");
+   scanf("%s",name);
+   printf("Enter your surname \n");
+   scanf("%s",surname);
+   printf("Enter your age \n");
+   scanf("%d",&age);
+   
+   printf(" Your name is %s, your surname is %s, and you are %d years old",name,surname, age);
+
+    return 0;
+}
